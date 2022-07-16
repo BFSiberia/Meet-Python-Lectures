8e60d2b5ee7ba2bba9d44c1c05ac83ba9ab9b6de
Alex <a.v.tarasov@gmail.com>
2022-07-16 12:10:20 +0800

    minor amendments for functions and recursion

--- Lecture 2/recursion.py
+++ Lecture 2/recursion.py
@@ -1,15 +1,18 @@
-def Fuctorial (number):
+# Факториал с выводом а экран
+def Factorial (number):
     if number in [1]:
         return 1
     else:
-        return number*Fuctorial(number-1)
+        return number*Factorial(number-1)
 
 list_fact =[]
 
 for i in range(1,10):
-    list_fact.append(Fuctorial(i))
+    list_fact.append(Factorial(i))
 print(list_fact)
 
+
+# Фибоначчи с выводом на экран
 def Fibo(number):
     if number in [1,2]:
         return 1
